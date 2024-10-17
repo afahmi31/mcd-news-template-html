e140f875c1f7ff945a2df96e006b73105eeeb5e0
Jmbr0919 <jembarfadillah@gmail.com>
2024-10-17 17:04:02 +0700

    video news

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./*.{html,js}",
-    "./src/**/*.{html,js}"],
+  content: ["./*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        bricolage: ['"Bricolage Grotesque"', "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
